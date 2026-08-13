617e7dd66c7fbefb931aee36f2b34161610ef41f
Heytor Pimentel <heytorpimentelnascimento@hotmail.com>
2026-08-13 15:51:41 -0300

    Add SQL script to insert products into database

---
+++ main/database/insereprodutos.sql
@@ -0,0 +1,9 @@
+-- 3. Acessa o banco de dados
+USE `loja`;
+
+-- 3. Realiza o INSERT adicionando 2 produtos
+INSERT INTO `PRODUTO` (`nome`, `valor`, `quantidade`) VALUES
+('Notebook', 3500.00, 10),
+('Mouse Gamer', 150.50, 45);
+
+
